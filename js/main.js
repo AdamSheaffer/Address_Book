70e720b2579f7162590390569c8bc10fe2c6361a
Stephania <stephania07>
2014-12-05 14:57:19 -0600

    add functionality works

--- js/main.js
+++ js/main.js
@@ -9,18 +9,21 @@
         {
           name: 'Adam Sheaffer',
           email: 'adam.e.sheaffer@gmail.com',
-          phone: 215-760-0392
+          phone: '215-760-0392'
         },
         {
           name: 'Stephania Iyob',
           email: 'StephaniaIyob@gmail.com',
-          phone: 202-569-3450
+          phone: '202-569-3450'
         }
       ];
 
       vm.addNewContact = function(){
         vm.contacts.push(vm.newContact);
+		 	  console.log("add new contact is firing. new info is:" + vm.newContact.name);
       }
+     
+     vm.newContact = {}
 
 
     })
