79bdb892d3393fd0d93d1b82bedcbfa856db09d9
Stephania <stephania07>
2014-12-06 15:28:11 -0600

    Add & Delete from Firebase

--- js/main.js
+++ js/main.js
@@ -2,49 +2,34 @@
   'use strict';
 
   angular.module('addressBook', [])
-    .controller('addressBookController', function(){
+    .controller('addressBookController', function($http){
       var vm = this;
-
-      vm.contacts = [
-        {
-          name: 'Adam Sheaffer',
-          email: 'adam.e.sheaffer@gmail.com',
-          phone: '215-760-0392',
-          image: 'https://avatars1.githubusercontent.com/u/9039241?v=3&s=460'
-        },
-        {
-          name: 'Stephania Iyob',
-          email: 'stephaniaiyob@gmail.com',
-          phone: '202-569-3450',
-          image: 'https://avatars3.githubusercontent.com/u/9039275?v=3&s=460'
-        },
-        {
-          name: 'Steve Jobs',
-          email: 'stevejobs@gmail.com',
-          phone: '123-456-789',
-          image: 'http://i.imgur.com/ZOjAIb.jpg'
-        },
-        {
-          name: 'Paulo Coelho',
-          email: 'pauloc@gmail.com',
-          phone: '123-456-789',
-          image: 'http://i.imgur.com/7jpxQxLb.jpg'
-        },
-
-      ];
       
+      $http.get('https://stephania-volunteer.firebaseio.com/contacts.json')
+      .success(function(data){
+        vm.contacts = data;
+       });
 
       vm.addNewContact = function(){
-        vm.contacts.push(vm.newContact);
-        vm.newContact = null;
-		 	  //console.log("add new contact is firing. new info is:" + vm.newContact.name);
+      $http.post('https://stephania-volunteer.firebaseio.com/contacts.json', vm.newContact)
+       .sucess(function(data){
+         vm.contacts[data.name] = vm.newContact;
+         vm.newContact = null;
+      	});
+        //vm.contacts.push(vm.newContact);
+        //vm.newContact = null;
+		//console.log("add new contact is firing. new info is:" + vm.newContact.name);
       };
      
-     vm.removeContact = function(contact){
-       var index = vm.contacts.indexOf(contact);
-       vm.contacts.splice(index, 1);
-
+     vm.removeContact = function(abId){
+       var url ="https://stephania-volunteer.firebaseio.com/" + abId + ".json";
+       $http.delet(url)
+       .success(function(){
+         delete vm.contacts[ab];
+       });
+       //var index = vm.contacts.indexOf(contact);
+       //vm.contacts.splice(index, 1);
      }
       vm.newContact = {}
-    })
+    });
 })();
